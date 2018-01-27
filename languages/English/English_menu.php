7f08ce7efaab8dc842a8f35bec4179cb59278322
Barry G4HDU <G4HDU@users.noreply.github.com>
2018-01-27 13:22:44 +0000

    Initial Commit

---
+++ languages/English/English_menu.php
@@ -0,0 +1,23 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        On This Day Menu for e107 v7xx - by Father Barry
+|
+|        This module for the e107 .7+ website system
+|        Copyright Barry Keal 2004-2008
+|
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|
++---------------------------------------------------------------+
+*/
+if (!defined('e107_INIT'))
+{
+    exit;
+}
+define(OTDLAN_CAP, "On This Day ...");
+define(OTD_MORE,"[more]");
+define(OTD_MONTHLIST,",January,February,March,April,May,June,July,August,September,October,November,December");
+
+// This is the string displayed if "showifempty" is set:
+define(OTDLAN_DEFAULT, "Nothing much happened.");
